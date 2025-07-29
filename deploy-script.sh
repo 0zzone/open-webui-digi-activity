eff3a5f937cf6c59bec8b0b9dc0e290a48b72f9b
Mattéo Bonnet <49744127+matt91320@users.noreply.github.com>
2025-07-30 00:32:42 +0200

    update du script de deploy

--- deploy-script.sh
+++ deploy-script.sh
@@ -1,8 +1,8 @@
-echo "✅ Building the image"
-docker build -t open-webui-digi-activity .
+echo "✅ Building the image" && \
+docker build -t open-webui-digi-activity . && \
 
-echo "✅ Tagging the image"
-docker tag open-webui-digi-activity:latest matt91320/open-webui-digi-activity:latest
+echo "✅ Tagging the image" && \
+docker tag open-webui-digi-activity:latest matt91320/open-webui-digi-activity:latest && \
 
-echo "✅ Pushing the image to Docker hub"
+echo "✅ Pushing the image to Docker Hub" && \
 docker push matt91320/open-webui-digi-activity:latest
