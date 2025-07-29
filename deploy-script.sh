ad05ddbbe2c4a55d11b9c79a30a3de3f77d2e85d
Mattéo Bonnet <49744127+matt91320@users.noreply.github.com>
2025-07-29 23:02:40 +0200

    Digi Activity assets

---
+++ deploy-script.sh
@@ -0,0 +1,8 @@
+echo "✅ Building the image"
+docker build -t open-webui-digi-activity .
+
+echo "✅ Tagging the image"
+docker tag open-webui-digi-activity:latest matt91320/open-webui-digi-activity:latest
+
+echo "✅ Pushing the image to Docker hub"
+docker push matt91320/open-webui-digi-activity:latest
